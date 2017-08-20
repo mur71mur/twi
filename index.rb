8cd8f8ef36482b159460fee9cc42b26a584e3f90
murase <30692889+mur71mur@users.noreply.github.com>
2017-08-21 00:50:45 +0900

    Update index.rb

--- index.rb
+++ index.rb
@@ -8,10 +8,14 @@ client = Twitter::REST::Client.new do |config|
 end
 
 #loop do
+	#タイムラインの新しいツイートの情報をtweetに入れる
 	client.home_timeline.take(1).each do |tweet|
+	#アカ名とツイート内容を表示
     	puts "#{tweet.user.screen_name}:"
     	puts "#{tweet.text}"
+		#ツイートのidを変数idにいれる
 		id = tweet.id.to_s
+	#そのidにファボしてリプ送る
     	client.favorite(id)
 		puts id
 		client.update("ハチフランシスコなう", in_reply_to_status_id: id, place_id: "df51dec6f4ee2b2c")
